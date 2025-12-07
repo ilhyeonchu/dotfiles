a4800bb6ed5f39a90989b115115055dc0b8f4cd1
ilhyeonchoo <ilhyeonchu@gmail.com>
2025-12-08 04:03:18 +0900

    Update wezterm.lua
    asfd

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder and wezterm.config_builder() or {}
 local act = wezterm.action
 local mux = wezterm.mux
 config.keys = {}
--- config.leader = { key = '`' }   -- 리더(prefix)키 설정
+config.leader = { key = 'b', mods = "CTRL" } -- 리더(prefix)키 설정
 
 --------------------------------------------------------------------------------------------
 -- ssh 설정
@@ -22,10 +22,10 @@ config.ssh_domains = {
 --------------------------------------------------------------------------------------------
 -- 기본 셀 설정
 --------------------------------------------------------------------------------------------
-config.default_prog = { "/bin/zsh", "-l" }
+-- config.default_prog = { "/bin/zsh", "-l" }
 
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", weight = "Bold" },
+	{ family = "JetBrains Mono",           weight = "Bold" },
 	{ family = "JetBrains Mono Nerd Font", weight = "Medium" },
 	{ family = "Noto Color Emoji" },
 })
@@ -34,8 +34,29 @@ config.font = wezterm.font_with_fallback({
 config.font_size = 13.0
 config.line_height = 1.10
 config.color_schemes = { MyPreset = mycolors }
--- config.color_scheme = "MyPreset"
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "MyPreset"
+--config.color_scheme = "Catppuccin Mocha"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = false
+local dimmer = { brightness = 0.1 }
+config.background = {
+	{
+		source = {
+			--File = 'C:\\Users\\ilhyeonchu\\.config\\wezterm\\brightbackground1.png',
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\Surges.png",
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\download62.png",
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\11kana.png",
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\download62.png",
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\sui.png",
+			--File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\hizuki2.png",
+			File = "C:\\Users\\ilhyeonchu\\.config\\wezterm\\hizuki3.png",
+		},
+		attachment = "Fixed",
+		opacity = 1.0,
+		hsb = dimmer,
+	}
+}
 
 --------------------------------------------------------------------------------------------
 -- util 함수
@@ -82,7 +103,7 @@ end)
 -- 키 바인딩
 --------------------------------------------------------------------------------------------
 config.keys = {
-	{ key = "`", mods = "LEADER", action = act.SendKey({ key = "`" }) }, -- '`' 백틱 문자 입력
+	--[[ { key = "`", mods = "LEADER", action = act.SendKey({ key = "`" }) }, -- '`' 백틱 문자 입력
 	-- 워크스테이션 변경
 	{
 		key = "1",
@@ -103,12 +124,31 @@ config.keys = {
 	{
 		key = "3",
 		mods = "LEADER",
-		action = act.SwitchToWorkspace({ name = "cg", spawn = {
-			domain = { DomainName = "myser" },
-		} }),
-	},
-	{ key = "9", mods = "LEADER", action = act.EmitEvent("activate-ide") },
+		action = act.SwitchToWorkspace({
+			name = "cg",
+			spawn = {
+				domain = { DomainName = "myser" },
+			}
+		}),
+	}, ]]
+	{ key = "9", mods = "LEADER",       action = act.EmitEvent("activate-ide") },
+	{ key = "t", mods = "LEADER",       action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "LEADER",       action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	{ key = "h", mods = "LEADER",       action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "LEADER",       action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "LEADER",       action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "LEADER",       action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "z", mods = "LEADER",       action = wezterm.action.TogglePaneZoomState },
+	{ key = "f", mods = "LEADER",       action = wezterm.action.ToggleFullScreen },
+	{ key = "v", mods = "LEADER",       action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "LEADER",       action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "o", mods = "LEADER",       action = wezterm.action.ActivatePaneDirection("Next") },
 }
 
 config.scrollback_lines = 20000
+
 return config
